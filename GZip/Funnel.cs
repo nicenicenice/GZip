1b04e17c48f620f56ebd1e3f55858bf42b919082
Apple User <user@MacBook-Air-Apple.local>
2017-08-29 00:29:37 +0300

    first commit

---
+++ GZip/Funnel.cs
@@ -0,0 +1,251 @@
+﻿using System;
+using System.IO;
+using System.IO.Compression;
+using System.Threading;
+
+namespace GZip
+{
+    public class Funnel
+    {
+        private static uint counter = 0;
+        private static uint numPartDataForWrite = 0;
+        private static int numThreads = Environment.ProcessorCount;
+        private static long bufferSize = 8192;
+        private static FileStream sourceFileStream;
+        private static FileStream destinationFileStream;
+        private static object _locker = new object();
+        private static bool isCompress;
+
+        private uint numPartDataContaint = 0;
+        private byte[] buffer;
+        private MemoryStream streamBuffer;
+        private Thread thread;
+
+        public static int NumThreads
+        {
+            get { return numThreads; }
+            set { numThreads = value; }
+        }
+
+        public Funnel()
+        {
+            streamBuffer = new MemoryStream();
+
+            if (isCompress)
+                thread = new Thread(startCompress);
+            else
+                thread = new Thread(startDecompress);
+            thread.Start();
+            thread.Join();
+        }
+
+        public static int Compress(string inputFile, string outputFile)
+        {
+            Console.WriteLine("Processing, Please Wait...");
+            try
+            {
+                sourceFileStream = new FileStream(@inputFile, FileMode.Open, FileAccess.Read);
+                destinationFileStream = new FileStream(@outputFile, FileMode.Append, FileAccess.Write);
+                isCompress = true;
+
+                Funnel[] compressors = new Funnel[NumThreads];
+                for (int i = 0; i < compressors.Length; ++i)
+                    compressors[i] = new Funnel();
+                return 0;
+            }
+            catch (FileNotFoundException e)
+            {
+                Console.WriteLine("File not found.");
+                Console.WriteLine(e.StackTrace);
+                return 1;
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e.StackTrace);
+                return 1;
+            }
+            finally
+            {
+                DisposeData();
+            }
+        }
+
+        public static int Decompress(string inputFile, string outputFile)
+        {
+            Console.WriteLine("Processing, Please Wait...");
+            try
+            {
+                sourceFileStream = new FileStream(@inputFile, FileMode.Open, FileAccess.Read);
+                destinationFileStream = new FileStream(@outputFile, FileMode.Append, FileAccess.Write);
+                isCompress = false;
+
+                Funnel[] decompressors = new Funnel[NumThreads];
+                for (int i = 0; i < decompressors.Length; ++i)
+                    decompressors[i] = new Funnel();
+                return 0;
+            }
+            catch (FileNotFoundException e)
+            {
+                Console.WriteLine("File not found.");
+                Console.WriteLine(e.StackTrace);
+                return 1;
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e.StackTrace);
+                return 1;
+            }
+            finally
+            {
+                DisposeData();
+            }
+        }
+
+        public void startCompress()
+        {
+            try
+            {
+                while (true)
+                {
+                    lock (_locker)
+                    {
+                        if (sourceFileStream.Length == sourceFileStream.Position)
+                        {
+                            streamBuffer.Close();
+                            return;
+                        }
+                        ToFillRawDataBuffer();
+                    }
+                    CompressPartData();
+                    WriteConvertedDataToFile();
+                }
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e.StackTrace);
+            }
+            finally
+            {
+                if (streamBuffer != null)
+                    streamBuffer.Close();
+            }
+        }
+
+        public void startDecompress()
+        {
+            try
+            {
+                while (true)
+                {
+                    lock (_locker)
+                    {
+                        if (sourceFileStream.Length == sourceFileStream.Position)
+                        {
+                            streamBuffer.Close();
+                            return;
+                        }
+                        ToFillDecompressedDataBuffer();
+                    }
+                    DecompressPartData();
+                    WriteConvertedDataToFile();
+                }
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e.StackTrace);
+            }
+            finally
+            {
+                if (streamBuffer != null)
+                    streamBuffer.Close();
+            }
+        }
+
+        private void ToFillRawDataBuffer()
+        {
+            long curBufferSize = bufferSize;
+            lock (_locker)
+            {
+                if (sourceFileStream.Length - sourceFileStream.Position < bufferSize)
+                    curBufferSize = (int)(sourceFileStream.Length - sourceFileStream.Position);
+            }
+
+            buffer = new byte[curBufferSize];
+
+            lock (_locker)
+            {
+                sourceFileStream.Read(buffer, 0, buffer.Length);
+                numPartDataContaint = counter++;
+            }
+        }
+
+        private void ToFillDecompressedDataBuffer()
+        {
+            // считываем длину порции данных
+            byte[] affix = new byte[8];
+            lock (_locker)
+                sourceFileStream.Read(affix, 0, 8);
+
+            int resBufferLength = BitConverter.ToInt32(affix, 4);
+            buffer = new byte[resBufferLength + 1];
+            affix.CopyTo(buffer, 0);
+
+            lock (_locker)
+            {
+                sourceFileStream.Read(buffer, 8, resBufferLength - 8);
+                numPartDataContaint = counter++;
+            }
+        }
+
+        private void CompressPartData()
+        {
+            streamBuffer.SetLength(0);
+            using (GZipStream compressionStream = new GZipStream(streamBuffer, CompressionMode.Compress, true))
+            {
+                compressionStream.Write(buffer, 0, buffer.Length);
+            }
+        }
+
+        private void DecompressPartData()
+        {
+            using (MemoryStream bufferStream = new MemoryStream(buffer))
+            {
+                using (GZipStream decompressionStream = new GZipStream(bufferStream, CompressionMode.Decompress))
+                {
+                    streamBuffer.SetLength(0);
+                    decompressionStream.CopyTo(streamBuffer);
+                }
+            }
+        }
+
+        private void WriteConvertedDataToFile()
+        {
+            byte[] convertedData = streamBuffer.ToArray();
+
+            while (true)
+            {
+                if (numPartDataForWrite == numPartDataContaint)
+                {
+                    lock (_locker)
+                    {
+                        // записать длину порции данных
+                        if (isCompress)
+                            BitConverter.GetBytes(convertedData.Length).CopyTo(convertedData, 4);
+
+                        destinationFileStream.Write(convertedData, 0, convertedData.Length);
+                        numPartDataForWrite++;
+                    }
+                    break;
+                }
+            }
+        }
+
+        private static void DisposeData()
+        {
+            if (destinationFileStream != null)
+                destinationFileStream.Close();
+            if (sourceFileStream != null)
+                sourceFileStream.Close();
+        }
+    }
+}
---
+++ GZip/Program.cs
@@ -0,0 +1,50 @@
+﻿using System;
+
+// 0. проверяем аргументы, переданные в командной строке
+// в соответствии с их значением, запускаем одну из двух функций: Compress или Decompress
+
+// 1. в каждой из этих функций создаем объекты типа Funnel, каждый объект инкапсулирует в себе поток два буффера 
+// число объектов равно числу процессоров на данной машине
+// в каждом из потоков запускаем соответствующие методы startCompress или startDecompress
+
+// 2. далее запускаем бесконечный цикл, до тех пор, пока не будут полностью прочитаны данные из исходного файла
+// в цикле поочередно запускаем 3-и функции, схожие по смыслу для компресси и для декомпрессии:
+//      - "заполнить буффер данными из исходного файла"
+//      - "сконвертировать данные (компрессия или декомпрессия)"
+//      - "записать, полученный результат, в файл назначения"
+// доступы к потокам ввода и вывода синхронизированы, параллельно происходит только преобразование данных
+
+namespace GZip
+{
+    public class Program
+    {
+        public static int Main(string[] args)
+        {
+            int result = 1;
+            try
+            {
+                if (args.Length < 3)
+                {
+                    throw new ArgumentException("Invalid arguments");
+                }
+                switch (args[0])
+                {
+                    case "compress":
+                        result = Funnel.Compress(@args[1], @args[2]);
+                        break;
+                    case "decompress":
+                        result = Funnel.Decompress(@args[1], @args[2]);
+                        break;
+                    default:
+                        throw new ArgumentException("Invalid arguments");
+                }
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e);
+            }
+
+            return 0;
+        }
+    }
+}
